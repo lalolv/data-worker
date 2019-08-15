2ed35d31bcf43359f1e342b7a93f8a5505c19f95
Lalo <code@lalo.im>
2019-08-15 17:20:54 +0800

    Add email; Add dict list.

--- handler/read.go
+++ handler/read.go
@@ -2,36 +2,62 @@ package handler
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math"
 	"os"
 	"time"
 
 	"github.com/gookit/gcli/v2/progress"
 	"github.com/lalolv/data-worker/workers"
+	"github.com/lalolv/goutil"
 )
 
 // LoadDicts Load dict by fields
 func LoadDicts(fields []interface{}, reCount float64, dictPath string) {
 	dictData = map[string][]string{}
+	// Read dict list file
+	dictMaps := readDictList(dictPath)
 	// for each field
 	for _, v := range fields {
 		val, ok := v.(map[string]interface{})
 		if ok {
 			if dict, ok := val["dict"]; ok {
-				fmt.Println("dict", dict)
+				// Get dict info from list
+				dictMap := dictMaps[dict.(string)].(map[string]interface{})
 				// read data
 				fieldsName := val["name"].(string)
-				fmt.Println("Loading data for ", fieldsName)
-				dataColl := ReadLines(reCount, 61.00, fmt.Sprintf("./%s/%s.txt", dictPath, dict.(string)))
+				fmt.Println("Loading data for", fieldsName)
+				// Read data
+				count, _ := goutil.ToFloat64(dictMap["count"])
+				dataColl := ReadLines(reCount, count, fmt.Sprintf("./%s/%s", dictPath, dictMap["file"]))
 				// Rand data
 				workers.ShuffleStrings(dataColl)
 				// Add to dict
 				dictData[fieldsName] = dataColl
-
 			}
 		}
 	}
+}
+
+func readDictList(dictPath string) map[string]interface{} {
+	listFile := fmt.Sprintf("./%s/.list.json", dictPath)
+	// Check path exist
+	if !CheckPathIsExist(listFile) {
+		fmt.Println("No find dict list file!")
+		return map[string]interface{}{}
+	}
+
+	data, err := ioutil.ReadFile(listFile)
+	if err != nil {
+		return map[string]interface{}{}
+	}
+
+	var dictMap map[string]interface{}
+	json.Unmarshal(data, &dictMap)
+
+	return dictMap
 }
 
 // ReadLines Read a few line
